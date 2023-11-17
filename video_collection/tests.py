7edacc49cdb166c9977f1fe665dc623127efb9ee
Richard Thies <richardjthies95@gmail.com>
2023-11-16 19:32:49 -0600

    testing the title message of app added

--- video_collection/tests.py
+++ video_collection/tests.py
@@ -1,3 +1,43 @@
 from django.test import TestCase
+from django.urls import reverse #converting name of url into the full path
 
-# Create your tests here.
+class TestHomePageMessage(TestCase):
+    def test_app_title_message_shown_on_home_page(self):#self always needed in a method that is part of a class
+        #Arrange?
+        url = reverse('home')#0:00-4:30?
+        #Act?
+        response = self.client.get(url)
+        #Assert?
+        self.assertContains(response, 'Starcraft Remastered')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
